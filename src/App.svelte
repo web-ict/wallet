59cce6c6584420c8021e2c78884c789de40f2930
Chris Dukakis <chrisdukakis@gmail.com>
2021-02-07 22:43:57 +0200

    update ui

--- src/App.svelte
+++ src/App.svelte
@@ -9,9 +9,12 @@
     
     export let name;
 
+    let ict
+    let cluster
     let hub
     let seed
     let seedTrits = new Int8Array(243)
+    let interval
     let transfers = []
     let balance = '0 i'
     let loggedIn = false
@@ -24,34 +27,48 @@
     let withdrawalAddress = ''
     let withdrawalValue = undefined
     let withdrawalError = undefined
+    let info = {}
+    let numberOfPeers = 0
+    let online
+
+    import('@web-ict/curl').then(({ Curl729_27 }) => {
+        ict = ICT({
+            autopeering: {
+                signalingServers: ['ws://116.203.136.136:8080', 'ws://116.203.136.136:8081', 'ws://116.203.136.136:8082'],
+                iceServers: [
+                    {
+                        urls: ['stun:stun3.l.google.com:19302'],
+                    },
+                ],
+                cooldownDuration:  10, // s
+                tiebreakerIntervalDuration:  10, // s
+                tiebreakerValue: Number.POSITIVE_INFINITY, // New transactions / second
+            },
+            dissemination: {
+                A: 1, // ms
+                B: 100,
+            },
+            subtangle: {
+                capacity: 1_000_000, // In transactions
+                pruningScale: 0.1, // In proportion to capacity
+                artificialLatency: 100, // ms
+            },
+            Curl729_27,
+        })
+        ict.launch()
+
+        const step = () => {
+            info = ict.info()
+            numberOfPeers = info.peers.filter(peer => peer.uptime > 0).length
+            online = numberOfPeers > 0
+            requestAnimationFrame(step)
+        }
+        requestAnimationFrame(step)
+    })
 
     function init() {
-        hub = import('@web-ict/curl').then(({ Curl729_27 }) => {
-            const ict = ICT({
-                autopeering: {
-                    signalingServers: ['ws://116.203.136.136:8080', 'ws://116.203.136.136:8081', 'ws://116.203.136.136:8082'],
-                    iceServers: [
-                        {
-                            urls: ['stun:stun3.l.google.com:19302'],
-                        },
-                    ],
-                    cooldownDuration:  10, // s
-                    tiebreakerIntervalDuration:  10, // s
-                    tiebreakerValue: Number.POSITIVE_INFINITY, // New transactions / second
-                },
-                dissemination: {
-                    A: 1, // ms
-                    B: 100,
-                },
-                subtangle: {
-                    capacity: 1_000_000, // In transactions
-                    pruningScale: 0.1, // In proportion to capacity
-                    artificialLatency: 100, // ms
-                },
-                Curl729_27,
-            })
-
-            const cluster = economicCluster({
+        import('@web-ict/curl').then(({ Curl729_27 }) => {
+            cluster = economicCluster({
                 intervalDuration: 5 * 1000,
                 ixi: ict.ixi,
                 Curl729_27,
@@ -64,28 +81,24 @@
                 weight: 1,
             })
 
-    
-            trytesToTrits(seed, seedTrits, 0, 243)
-
             const iss = ISS(Curl729_27)
-            const persistenceIdTrits = iss.addressFromDigests(iss.digests(iss.key(iss.subseed(seedTrits, 0), 2)))
+            const persistenceId = trytes(iss.addressFromDigests(iss.digests(iss.key(iss.subseed(seedTrits, 0), 2))), 0, ADDRESS_LENGTH)
 
             hub = HUB({
                 seed: seedTrits,
                 security: 2,
                 persistencePath: './',
-                persistenceId: trytes(persistenceIdTrits, 0, persistenceIdTrits.length),
+                persistenceId,
                 reattachIntervalDuration: 30 * 1000,
                 acceptanceThreshold: 100,
                 Curl729_27,
                 ixi: ict.ixi,
             })
 
-            ict.launch()
             cluster.launch()
             hub.launch()
 
-            const interval = setInterval(async () => {
+            interval = setInterval(async () => {
                 transfers = []
                 const { getTransfers, getBalance } = await hub
                 getTransfers().forEach(transfer => {
@@ -102,9 +115,18 @@
             }, 1000)
 
             loggedIn = true
-
-            return hub
         })   
+    }
+
+    function logout () {
+        seed = undefined
+        seedTrits = new Int8Array(243)
+        loggedIn = false
+        clearInterval(interval)
+        transfers = []
+        transfers = transfers
+        cluster.terminate()
+        hub.terminate()
     }
 
     function updateSeedChecksum () {
@@ -187,21 +209,39 @@
 </script>
 
 <header>
+    <div>
     <img id="logo" src="/logo.png">
+    </div>
     {#if loggedIn}
-        <div id="balance">Balance: {balance}</div>
         <div id="actions">
-            <button id="receive-button" on:click={() => (depositDialogVisible = true)}>Receive</button>
-            <button id="send-button" on:click={() => (withdrawDialogVisible = true)}>Send</button>
+            <button id="receive-button" class="button" on:click={() => (depositDialogVisible = true)}>Receive</button>
+            <button id="send-button" class="button" on:click={() => (withdrawDialogVisible = true)}>Send</button>
+            <div id="balance">
+                <div>Balance:</div>
+                <div id="balance-value">{balance}</div>
+            </div>
+            <div id="info">
+                <div id="peers"><span id="online-{online}"></span> {numberOfPeers}/3 peers</div>
+            </div>
+            <div id="secondary-actions">
+                <button class="button-2">Cluster settings</button>
+                <button class="button-2" on:click={logout}>Logout</button>
+            </div>
+        </div>
+    {:else}
+        <div id="info">
+            <div id="peers"><span id="online-{online}"></span> {numberOfPeers}/3 peers</div>
         </div>
     {/if}
 </header>
 <main>
     {#if !loggedIn}
-        <label for="seed">Seed</label>
-        <input name="seed" type="password" bind:value={seed} on:input={updateSeedChecksum}>
-        <span id="seed-checksum">{seedChecksum}</span>
-        <button disabled='{seed === undefined}' on:click={init}>Login</button>
+        <form>
+            <label for="seed">Seed</label>
+            <input name="seed" type="password" size="81" autofocus bind:value={seed} on:input={updateSeedChecksum}>
+            <span id="seed-checksum">{seedChecksum}</span>
+            <input type="submit" class="button" disabled='{seed === undefined}' on:click={init} value="Login">
+        </form>
     {/if}
     {#each transfers as transfer}
 		<div class="transfer">
@@ -215,23 +255,25 @@
                 <h2>Receive</h2>
                 <div>
                     {#if depositStep === 0}
-                        <label for="deposit-value">Value:</label>
-                        <input name="deposit-value" bind:value={depositValue} type="number" min="0">
-                        <select name="deposit-unit" bind:value={depositUnit} class="unit">
-                            <option value="i">i</option>
-                            <option value="Ki">Ki</option>
-                            <option value="Mi">Mi</option>
-                            <option value="Gi">Gi</option>
-                            <option value="Ti">Ti</option>
-                            <option value="Pi">Pi</option>
-                        </select>
-                        <button disabled='{depositValue === 0}' on:click={deposit}>Next ❯</button>
+                        <form>
+                            <label for="deposit-value">Value:</label>
+                            <input name="deposit-value" bind:value={depositValue} type="number" min="0">
+                            <select name="deposit-unit" bind:value={depositUnit} class="unit">
+                                <option value="i">i</option>
+                                <option value="Ki">Ki</option>
+                                <option value="Mi">Mi</option>
+                                <option value="Gi">Gi</option>
+                                <option value="Ti">Ti</option>
+                                <option value="Pi">Pi</option>
+                            </select>
+                            <input type="submit" class="button" disabled='{depositValue === 0}' on:click={deposit} value="Next ❯">
+                        </form>
                     {:else}
                         <div transition:fly="{{ y: 30, duration: 300 }}">
                             <p>Address should be used only once.</p>
                             <p id="deposit-address">{depositAddress}</p>
-                            <button on:click={resetDepositDialog}>Close</button>
-                            <button on:click={copyDepositAddress}>Copy</button>
+                            <button class="button" on:click={resetDepositDialog}>Close</button>
+                            <button class="button" on:click={copyDepositAddress}>Copy</button>
                         </div>
                     {/if}
                 </div>
@@ -244,26 +286,23 @@
                 <h2>Send</h2>
                 {#if withdrawStep === 0}
                     <div id="withdrawal-step-1">
-                        <label for="withdrawal-address">Address:</label>
-                        <input name="withdrawal-address" id="withdrawal-address" bind:value={withdrawalAddress} type="text">
-                        <button disabled='{withdrawalAddress === ''}' on:click={getWithdrawalValue}>Next ❯</button>
+                        <form>
+                            <label for="withdrawal-address">Address:</label>
+                            <input name="withdrawal-address" size="81" autocomplete="off" bind:value={withdrawalAddress} type="text">
+                            <button class="button" disabled='{withdrawalAddress === ''}' on:click={getWithdrawalValue}>Next ❯</button>
+                        </form>
                     </div>
+                {:else if withdrawalError !== undefined}
+                    <p>{withdrawalError}</p>
+                    <button class="button" on:click={resetWithdrawDialog}>Close</button>
+                {:else if withdrawalValue !== undefined}
+                    <p>About to send {formatValueWithHumanReadableUnit(withdrawalValue)} to:</p>
+                    <p>{withdrawalAddress}</p>
+                    <button class="button" on:click={resetWithdrawDialog}>Cancel</button>
+                    <button class="button" on:click={withdraw}>Send ❯</button>
                 {:else}
-                    <div id="withdrawal-step-2">
-                        {#if withdrawalError !== undefined}
-                            <p>{withdrawalError}</p>
-                            <button on:click={resetWithdrawDialog}>Close</button>
-                        {:else if withdrawalValue !== undefined}
-                            <p>About to send {formatValueWithHumanReadableUnit(withdrawalValue)} to:</p>
-                            <p>{withdrawalAddress}</p>
-                            <button on:click={resetWithdrawDialog}>Cancel</button>
-                            <button on:click={withdraw}>Send ❯</button>
-                        {:else}
-                            <p>Transfer has not been initiated.</p>
-                            <button on:click={resetWithdrawDialog}>Close</button>
-                        {/if}
-
-                    </div>
+                    <p>Transfer has not been initiated.</p>
+                    <button class="button" on:click={resetWithdrawDialog}>Close</button>
                 {/if}
             </div>
         </div>
@@ -273,23 +312,24 @@
 <style>
     header {
         width: 100%;
-        height: 2.5em;
-        padding: 1em 0em;
+        padding: 0 2.5em;
+        height: 5em;
+        box-sizing: border-box;
         position: fixed;
         top: 0;
         left: 0;
         background: #000;
         display: flex;
         justify-content: space-between;
+        align-items: center;
     }
     
     main {
         padding: 1em 2em;
         width: 100%;
-        margin: 2em auto 0 auto;
+        margin: 6em auto 0 auto;
         position: relative;
         top: 60px;
-        font-family: monospace;
         box-sizing: border-box;
     }
 
@@ -304,30 +344,68 @@
     }
 
     #logo {
-        height: 2.5em;
-        padding-left: 2em;
+        height: 3em;
         filter: invert();
+        align-self: center;
     }
 
     #balance {
-        font-size: 1.5em;
-        padding-top: 0.5em;
+        font-size: 1em;
+        align-self: center;
+        padding: 0 1em;
+        flex-direction: column;
+    }
+
+    #balance-value {
         color: #fff;
-        font-family: monospace;   
+        font-size: 1.5em;
     }
 
     #actions {
-        padding-right: 2em;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    #secondary-actions {
+        display: flex;
+        align-items: center;
+    }
+
+    #info {
+        font-size: 1em;
+        align-self: center;
+        flex-direction: column;
+    }
+
+    #peers {
+        padding-right: 1em;
+    }
+
+    #online-false {
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        background: #555;
+        border-radius: 10px;
+        transition: background-color 1s;
+    }
+
+    #online-true {
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        background: rgb(21, 202, 27);
+        border-radius: 10px;
+        transition: background-color 1s;
     }
 
     #receive-button {
         margin-right: 0.5em;
     }
 
-    button {
-        padding: 0.6em 1em;
+    .button {
+        padding: 0.3em 0.8em;
         font-size: 1.5em;
-        font-family: monospace;
         color: #fff;
         border: 0;
         border-radius: 1.2em;
@@ -336,10 +414,19 @@
         transition: background-color 0.3s ease, color 0.3s ease;
     }
 
-
-    button:hover, button:focus {
+    .button:hover {
         color: #000;
         background: #fff;
+    }
+
+    .button-2 {
+        background: transparent;
+        border: 0;
+        color: rgba(255,255,255,.7)
+    }
+
+    .button-2:hover {
+        text-decoration: underline;
     }
 
     .card {
@@ -358,14 +445,12 @@
         font-weight: 300;
         margin: 0;
         padding-bottom: 1em;
-        font-family: monospace;
     }
 
     input, select {
         padding: 0.6em 0em;
         margin: 0.2em;
-        font-family: monospace;
-        font-size: 1.5em;
+        font-size: 1.2em;
         border: 0;
         border-bottom: 2px solid rgba(255,255,255,0.5);
         background: transparent;
@@ -377,10 +462,6 @@
 
     input:focus {
         outline: none;
-    }
-
-    #withdrawal-address {
-        width: 800px;
     }
 
     .dialog {
